4a588665843fede19398671480f0dba0bb00bf74
serious-chen <serious_chen@icloud.com>
2021-02-11 19:30:05 +0800

    更新版本

--- springCloudLearning-web/src/main/java/myPain/AppMain.java
+++ springCloudLearning-web/src/main/java/myPain/AppMain.java
@@ -5,11 +5,10 @@ import com.netflix.loadbalancer.RandomRule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -24,10 +23,6 @@ import org.springframework.web.client.RestTemplate;
 public class AppMain {
 
 
-    // 启动的时候要注意，由于我们在controller中注入了RestTemplate，所以启动的时候需要实例化该类的一个实例
-    @Autowired
-    private RestTemplateBuilder builder;
-
     @Bean
     public IRule ribbonRule(){
         return new RandomRule();
@@ -37,7 +32,7 @@ public class AppMain {
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
-        return builder.build();
+        return new RestTemplate();
     }
 
     public static void main(String[] args) {
