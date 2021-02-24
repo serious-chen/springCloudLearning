ad00bbab39ba720edffe5d41bbef5a63f5e6d7e4
serious-chen <chenquanqing@witenet.com>
2021-02-24 18:43:56 +0800

    加入消息总线；docker配置初始化

---
+++ eureka/src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting springCloudLearning "
+echo "********************************************************"
+java -jar /usr/local/springCloudLearning/@project.build.finalName@.jar
--- springCloudLearning-web/src/main/java/myPain/controller/TestController.java
+++ springCloudLearning-web/src/main/java/myPain/controller/TestController.java
@@ -4,8 +4,10 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
 import myPain.config.EurekaClientFeign;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
  * @Date 2021/2/4 9:26 下午
  **/
 @Slf4j
+@RefreshScope
 @RestController
 public class TestController {
 
@@ -28,6 +31,9 @@ public class TestController {
     @Autowired
     private EurekaClientFeign eurekaClientFeign;
 
+    @Value("${app.version}")
+    private String version;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     @ResponseBody
     public String hello(){
@@ -36,5 +42,11 @@ public class TestController {
         /*ResponseEntity<String> result = restTemplate.getForEntity("http://user/hello", String.class);
         log.info(result.getBody());*/
         return eurekaClientFeign.sayHiFromClientEureka("hi");
+    }
+
+    @RequestMapping(value = "/version", method = RequestMethod.GET)
+    @ResponseBody
+    public String version(){
+        return version;
     }
 }
